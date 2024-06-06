5f7d29d68a5be694321b09f398e1f21e943281c7
Tyler Barnes <septr@live.com>
2024-06-06 00:16:15 -0500

    Updated Files

--- cores/r_ATmega328P.h
+++ cores/r_ATmega328P.h
@@ -7,80 +7,80 @@ construct_ISR(PCINT2_vect)
 void r_setup(uint8_t _pin) {
 	switch(_pin) {
 	case 0:
-		PCICR = PCIE2;
-		PCMSK2 = PCINT17;
+		PCICR |= PCIE2;
+		PCMSK2 |= PCINT17;
 		break;
 	case 1:
-		PCICR = PCIE2;
-		PCMSK2 = PCINT16;
+		PCICR |= PCIE2;
+		PCMSK2 |= PCINT16;
 		break;
 	case 2:
-		PCICR = PCIE2;
-		PCMSK2 = PCINT18;
+		PCICR |= PCIE2;
+		PCMSK2 |= PCINT18;
 		break;
 	case 3:
-		PCICR = PCIE2;
-		PCMSK2 = PCINT19;
+		PCICR |= PCIE2;
+		PCMSK2 |= PCINT19;
 		break;
 	case 4:
-		PCICR = PCIE2;
-		PCMSK2 = PCINT20;
+		PCICR |= PCIE2;
+		PCMSK2 |= PCINT20;
 		break;
 	case 5:
-		PCICR = PCIE2;
-		PCMSK2 = PCINT21;
+		PCICR |= PCIE2;
+		PCMSK2 |= PCINT21;
 		break;
 	case 6:
-		PCICR = PCIE2;
-		PCMSK2 = PCINT22;
+		PCICR |= PCIE2;
+		PCMSK2 |= PCINT22;
 		break;
 	case 7:
-		PCICR = PCIE2;
-		PCMSK2 = PCINT23;
+		PCICR |= PCIE2;
+		PCMSK2 |= PCINT23;
 		break;
 	case 8:
-		PCICR = PCIE0;
-		PCMSK0 = PCINT0;
+		PCICR |= PCIE0;
+		PCMSK0 |= PCINT0;
 		break;
 	case 9:
-		PCICR = PCIE0;
-		PCMSK0 = PCINT1;
+		PCICR |= PCIE0;
+		PCMSK0 |= PCINT1;
 		break;
 	case 10:
-		PCICR = PCIE0;
-		PCMSK0 = PCINT2;
+		PCICR |= PCIE0;
+		PCMSK0 |= PCINT2;
 		break;
 	case 11:
-		PCICR = PCIE0;
-		PCMSK0 = PCINT3;
+		PCICR |= PCIE0;
+		PCMSK0 |= PCINT3;
 		break;
 	case 12:
-		PCICR = PCIE0;
-		PCMSK0 = PCINT4;
+		PCICR |= PCIE0;
+		PCMSK0 |= PCINT4;
 		break;
 	case 13:
-		PCICR = PCIE0;
-		PCMSK0 = PCINT5;
+		PCICR |= PCIE0;
+		PCMSK0 |= PCINT5;
 		break;
 	case 14:
-		PCICR = PCIE1;
-		PCMSK1 = PCINT9;
+		PCICR |= PCIE1;
+		PCMSK1 |= PCINT9;
 		break;
 	case 15:
-		PCICR = PCIE1;
-		PCMSK1 = PCINT10;
+		PCICR |= PCIE1;
+		PCMSK1 |= PCINT10;
 		break;
 	case 16:
-		PCICR = PCIE1;
-		PCMSK1 = PCINT11;
+		PCICR |= PCIE1;
+		PCMSK1 |= PCINT11;
 		break;
 	case 17:
-		PCICR = PCIE1;
-		PCMSK1 = PCINT12;
+		PCICR |= PCIE1;
+		PCMSK1 |= PCINT12;
 		break;
 	case 18:
-		PCICR = PCIE1;
-		PCMSK1 = PCINT13;
+		PCICR |= PCIE1;
+		PCMSK1 |= PCINT13;
 	default:
 		break;
 	}
