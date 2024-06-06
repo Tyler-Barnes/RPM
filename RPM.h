922466c0d4a4d23eed0aad0fe1da25a10057676c
Tyler Barnes <59125882+Tyler-Barnes@users.noreply.github.com>
2024-06-06 10:27:55 -0500

    Update RPM.h

--- RPM.h
+++ RPM.h
@@ -27,6 +27,8 @@ uint8_t r_pin;
 # include <cores/r_ATmega32U4.h>
 #elif defined(__AVR_ATmega328P__)
 # include <cores/r_ATmega328P.h>
+#elif defined(__AVR_ATmega2560__)
+# include <cores/r_ATmega2560.h>
 #else
 # error Your archetecture is not yet supported.
 #endif
