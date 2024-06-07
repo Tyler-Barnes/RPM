9033af859d68b2fa5157049d9184de0688393550
Tyler Barnes <septr@live.com>
2024-06-06 19:38:20 -0500

    Update r_ATmega32U4.h

--- cores/r_ATmega32U4.h
+++ cores/r_ATmega32U4.h
@@ -6,36 +6,36 @@ construct_ISR(PCINT0_vect)
 void r_setup(uint8_t _pin) {
 	switch(_pin) {
 	case 8:
-		PCICR |= PCIE0;
-		PCMSK0 |= PCINT4;
+		PCICR |= _BV(PCIE0);
+		PCMSK0 |= _BV(PCINT4);
 		break;
 	case 9:
-		PCICR |= PCIE0;
-		PCMSK0 |= PCINT5;
+		PCICR |= _BV(PCIE0);
+		PCMSK0 |= _BV(PCINT5);
 		break;
 	case 10:
-		PCICR |= PCIE0;
-		PCMSK0 |= PCINT6;
+		PCICR |= _BV(PCIE0);
+		PCMSK0 |= _BV(PCINT6);
 		break;
 	case 11:
-		PCICR |= PCIE0;
-		PCMSK0 |= PCINT7;
+		PCICR |= _BV(PCIE0);
+		PCMSK0 |= _BV(PCINT7);
 		break;
 	case 14:
-		PCICR |= PCIE0;
-		PCMSK0 |= PCINT3;
+		PCICR |= _BV(PCIE0);
+		PCMSK0 |= _BV(PCINT3);
 		break;
 	case 16:
-		PCICR |= PCIE0;
-		PCMSK0 |= PCINT1;
+		PCICR |= _BV(PCIE0);
+		PCMSK0 |= _BV(PCINT1);
 		break;
 	case 16:
-		PCICR |= PCIE0;
-		PCMSK0 |= PCINT2;
+		PCICR |= _BV(PCIE0);
+		PCMSK0 |= _BV(PCINT2);
 		break;
 	case 17:
-		PCICR |= PCIE0;
-		PCMSK0 |= PCINT0;
+		PCICR |= _BV(PCIE0);
+		PCMSK0 |= _BV(PCINT0);
 		break;
 	default:
 		break;
