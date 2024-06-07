18920bfe164e6bc385317f86c726672595ba4ff7
Tyler Barnes <septr@live.com>
2024-06-06 22:39:21 -0500

    Updates

--- RPM.h
+++ RPM.h
@@ -1,23 +1,23 @@
 /*
-    Written By: Tyler Barnes 
+    Written By: Tyler Barnes
     2024
 
-    No warranty expressed or implied. 
-*/
+    No warranty expressed or implied.
+ */
 
 #ifndef RPM_h
 #define RPM_h
 
 volatile uint32_t r_cpms = 0;
-uint32_t r_elapsed; 
+uint32_t r_elapsed;
 uint8_t r_pin;
 
 #define incRPM() {r_cpms++;}
 
 #define construct_ISR(vect)         \
-    ISR(vect) {                     \
-        incRPM();                   \
-    }
+	ISR(vect) {                     \
+		incRPM();                   \
+	}
 
 #if defined(__AVR_ATmega32U4__)
 # include <cores/r_ATmega32U4.h>
@@ -31,41 +31,27 @@ uint8_t r_pin;
 
 
 class RPMclass {
-public:
-	uint8_t numOfSamples = 1; 
-
 private:
-	uint32_t arr_samples[100];
-	uint8_t index = 0; 
-	void addSample(uint32_t _sample) {
-		arr_samples[index] = _sample; 
-		if (++index > numOfSamples) index = 0; 
-	}
-	uint32_t avgSample(){
-		uint32_t sum = 0; 
-		for (int i = 0; i < numOfSamples; i++) {
-			sum += arr_samples[i];
-		}
-		return sum / numOfSamples; 
-	}
+	uint16_t bufferSize = 1000;
 public:
-	void samples(uint8_t _val){
-		numOfSamples = _val; 
+	void pin(uint8_t _pin) {
+		r_pin = _pin;
+		pinMode(_pin, INPUT);
+		r_setup(_pin);
 	}
 
-    void pin(uint8_t _pin) {
-        r_pin = _pin;
-        pinMode(_pin, INPUT);
-        r_setup(_pin); 
-    }
+	uint32_t get() {
+		uint32_t RPM = (r_cpms * 30000) / (millis() - r_elapsed);
+		if (millis() - r_elapsed > bufferSize) {
+			r_elapsed = millis();
+			r_cpms = 0;
+		}
+		return RPM;
+	}
 
-    uint32_t get() {
-    	uint32_t RPM = (r_cpms * 30000) / (millis() - r_elapsed);
-    	addSample(RPM); 
-        r_elapsed = millis();
-        r_cpms = 0;
-        return avgSample();
-    }
+	void buffer(uint16_t _size){
+		bufferSize = _size; 
+	}
 };
 
 static RPMclass RPM;
