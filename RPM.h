ba94947eb3f95ef190d49510a388cccc8927c5d5
Tyler Barnes <septr@live.com>
2024-06-07 20:43:47 -0500

    updates

--- RPM.h
+++ RPM.h
@@ -17,15 +17,7 @@ volatile uint32_t r_cpms = 0;
         incRPM();                   \
     }
 
-#if defined(__AVR_ATmega32U4__)
-# include <cores/r_ATmega32U4.h>
-#elif defined(__AVR_ATmega328P__)
-# include <cores/r_ATmega328P.h>
-#elif defined(__AVR_ATmega2560__)
-# include <cores/r_ATmega2560.h>
-#else
-# error Your archetecture is not yet supported.
-#endif
+#include <cores.h>
 
 
 class RPMclass {
