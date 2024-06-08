5cb67174363672b5e8bfa2079e8d6f4d68aba5d0
Tyler Barnes <septr@live.com>
2024-06-08 15:07:12 -0500

    updates

--- RPM.h
+++ RPM.h
@@ -8,13 +8,16 @@
 #ifndef RPM_h
 #define RPM_h
 
-// volatile uint32_t r_cpms = 0;
+volatile uint32_t r_intMicros; 
 volatile uint32_t r_cpms1 = 0;
 volatile uint32_t r_cpms2 = 0;
 volatile uint32_t *r_cpms[2] = {&r_cpms1, &r_cpms2};
 #define DYNAMIC -1
 
-#define incRPM() {r_cpms1++;r_cpms2++;}
+#define incRPM() {			\
+	r_intMicros = micros(); \
+	r_cpms1++; r_cpms2++;	\
+}
 
 #define construct_ISR(vect)         \
     ISR(vect) {                     \
@@ -25,18 +28,20 @@ volatile uint32_t *r_cpms[2] = {&r_cpms1, &r_cpms2};
 
 class RPMclass {
 private:
-	int bufferMode = DYNAMIC; 
-    int bufferSize = 0;
-    uint8_t trigger = 1; 
+	long bufferSize = 0;	
+	int bufferMode = DYNAMIC;	
+	uint8_t nSamples = 5;
+	uint8_t	index = 0;
+	uint8_t trigger = 1; 
+	uint8_t active = 0;
+	uint16_t aSamples[100] = {0};
     uint32_t delta1 = 0; 
     uint32_t delta2 = 0; 
-    uint32_t *delta [2] = {&delta1, &delta2}; 
-    uint16_t duration1, duration2; 
-	uint16_t *duration[2] = {&duration1, &duration2};
-	uint8_t active = 0;
-	uint16_t aSamples[100]; 
-	uint8_t nSamples = 10; 
-	uint8_t index = 0; 
+    uint32_t *aDelta [2] = {&delta1, &delta2}; 
+    uint32_t duration1, duration2; 
+	uint32_t *duration[2] = {&duration1, &duration2};
+	uint32_t RPM;
+	uint32_t intMicros;
 
 	void addSample(uint16_t _sample) {
 		if (++index > nSamples) index = 0;
@@ -50,6 +55,13 @@ private:
 		}
 		return sum / nSamples; 
 	}
+
+	void fillArr(uint32_t _rpm) {
+		for (int i = 0; i < nSamples; i++) {
+			aSamples[i] = _rpm; 
+		}
+	}
+
 public:
     void pin(uint8_t _pin) {
         pinMode(_pin, INPUT);
@@ -57,35 +69,42 @@ public:
     }
 
     uint32_t get() {
-    	*duration[active] = (millis() - *delta[active]);
-        uint32_t RPM = (*r_cpms[active] * 30000) / *duration[active];
-        addSample(RPM); 
+    	intMicros = r_intMicros; 
+    	*duration[active] = (intMicros - *aDelta[active]);
+        RPM = round((double)(*r_cpms[active] * 30000000.0) / (double)*duration[active]);
+        if (avgSamples() == 0) {
+        	fillArr(RPM); 
+        } else {
+	        addSample(RPM); 
+        }
+
         if (bufferMode == DYNAMIC) {
-	        if (RPM > 10000) bufferSize = 0; 
-	        else if (RPM > 5000) bufferSize = 500;
-	        else if (RPM > 1000) bufferSize = 1000;
-	        else if (RPM > 500) bufferSize = 2000;
-	        else bufferSize = 3000;
+			if (RPM > 10000) bufferSize = 0; 
+	        else if (RPM > 5000) bufferSize = 500000;
+	        else if (RPM > 1000) bufferSize = 1000000;
+	        else if (RPM > 500000) bufferSize = 2000000;
+	        else bufferSize = 3000000;
 	    }
+
         if (*duration[active] > bufferSize / 2 && trigger) {
-        	*delta[active^1] = millis();
+        	*aDelta[active^1] = intMicros;
         	*r_cpms[active^1] = 0; 
         	trigger = 0; 
         }
         if (*duration[active] > bufferSize) {
             active ^= 1; 
             trigger = 1; 
-        }
+        } 
         return avgSamples();
-    }
-
-    void buffer(int _size){
-        bufferSize = _size; 
-        bufferMode = _size; 
     }
 
     void samples(uint8_t _samples) {
     	nSamples = _samples; 
+    }
+
+    void buffer( int _size) {
+    	bufferSize = (long)_size * 1000l;
+    	bufferMode = _size;  
     }
 };
 
