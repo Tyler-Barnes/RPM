602608df3fa8041aeaaf8c643f4e52b442c8da05
Tyler Barnes <septr@live.com>
2024-06-08 04:11:33 -0500

    added dynamic split buffering

--- RPM.h
+++ RPM.h
@@ -8,9 +8,13 @@
 #ifndef RPM_h
 #define RPM_h
 
-volatile uint32_t r_cpms = 0;
+// volatile uint32_t r_cpms = 0;
+volatile uint32_t r_cpms1 = 0;
+volatile uint32_t r_cpms2 = 0;
+volatile uint32_t *r_cpms[2] = {&r_cpms1, &r_cpms2};
+#define DYNAMIC -1
 
-#define incRPM() {r_cpms++;}
+#define incRPM() {r_cpms1++;r_cpms2++;}
 
 #define construct_ISR(vect)         \
     ISR(vect) {                     \
@@ -20,10 +24,18 @@ volatile uint32_t r_cpms = 0;
 #include <cores/cores.h>
 
 
+
 class RPMclass {
 private:
-    uint16_t bufferSize = 0;
-    uint32_t elapsed = 0; 
+	int bufferMode = DYNAMIC; 
+    int bufferSize = 0;
+    uint8_t trigger = 1; 
+    uint32_t delta1 = 0; 
+    uint32_t delta2 = 0; 
+    uint32_t *delta [2] = {&delta1, &delta2}; 
+    uint16_t duration1, duration2; 
+	uint16_t *duration[2] = {&duration1, &duration2};
+	uint8_t active = 0;
 public:
     void pin(uint8_t _pin) {
         pinMode(_pin, INPUT);
@@ -31,16 +43,30 @@ public:
     }
 
     uint32_t get() {
-        uint32_t RPM = (r_cpms * 30000) / (millis() - elapsed);
-        if (millis() - elapsed > bufferSize) {
-            elapsed = millis();
-            r_cpms = 0;
+    	*duration[active] = (millis() - *delta[active]);
+        uint32_t RPM = (*r_cpms[active] * 30000) / *duration[active];
+
+        if (bufferMode == DYNAMIC) {
+	        if (RPM > 10000) bufferSize = 0; 
+	        else if (RPM > 5000) bufferSize = 500;
+	        else if (RPM > 1000) bufferSize = 1000;
+	        else bufferSize = 2000;
+	    }
+        if (*duration[active] > bufferSize / 2 && trigger) {
+        	*delta[active^1] = millis();
+        	*r_cpms[active^1] = 0; 
+        	trigger = 0; 
+        }
+        if (*duration[active] > bufferSize) {
+            active ^= 1; 
+            trigger = 1; 
         }
         return RPM;
     }
 
-    void buffer(uint16_t _size){
+    void buffer(int _size){
         bufferSize = _size; 
+        bufferMode = _size; 
     }
 };
 
