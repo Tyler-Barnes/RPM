6273d3e383ac643f56a78f47b4b1d90ff5b8d9fb
Tyler Barnes <septr@live.com>
2024-06-14 01:26:30 -0500

    updates

--- RPM.h
+++ RPM.h
@@ -12,7 +12,7 @@ volatile uint16_t r_reset = 0;
 
 #include <cores/cores.h>
 
-#define hz2us (1.0 / (F_CPU * 1.001 / r_PRSCLR)) * 1000000
+#define hz2us (1.0 / (F_CPU * 1.0005 / r_PRSCLR)) * 1000000
 #define cpus(x) 1.0 / (hz2us * x)
 
 class RPMclass{
