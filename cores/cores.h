4cff664cf5a922ee4a646207735a26863f7ceed2
Tyler Barnes <septr@live.com>
2024-06-15 16:20:23 -0500

    Update cores.h

--- cores/cores.h
+++ cores/cores.h
@@ -4,8 +4,6 @@
 # include <cores/ATmega328P.h>
 #elif defined(__AVR_ATmega2560__)
 # include <cores/ATmega2560.h>
-#elif defined(__AVR_ATtiny402__)
-# include <cores/ATtiny402.h>
 #else
 # error Your archetecture is not yet supported.
 #endif
