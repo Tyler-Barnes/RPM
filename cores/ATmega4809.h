98c16c9a1ff7e74e2dc9a181014b9e939f7e9f2d
Tyler Barnes <septr@live.com>
2024-06-16 15:45:32 -0500

    Update ATmega4809.h

--- cores/ATmega4809.h
+++ cores/ATmega4809.h
@@ -21,17 +21,17 @@ ISR(TCB0_INT_vect){
 }
 
 uint16_t RPMclass::getRPM(){
-    r_incTimeOut();     // if interrupt stops firing, then also detect 0 RPM
+    r_incTimeout();     // if interrupt stops firing, then also detect 0 RPM
     return (r_overFlow) ? 0 : r_rpm(r_capture);
 }
 
 void RPMclass::config() {
-    CLKCTRL.MCLKCTRLA = 0x00;                       // internal 20MHz clock
-    CLKCTRL.MCLKCTRLB = 0x00;                       // no prescaler
-    EVSYS.CHANNEL4 = EVSYS_GENERATOR_PORT1_PIN4_gc; // Pin 6 (PF4) set as capture pin
-    EVSYS.USERTCB0 = EVSYS_CHANNEL_CHANNEL4_gc;     // TCB0 user connected to event channel4
-    TCB0.EVCTRL = 0x11;                             // enable input capture on falling edge 
-    TCB0.INTCTRL = 0x01;                            // enable input capture interrupt
-    TCB0.CTRLB = TCB_CNTMODE_FRQ_gc;                // frequency measurement mode               
-    TCB0.CTRLA = TCB_CLKSEL_CLKTCA_gc;              // use TCA timer prescaler 64
+    // CLKCTRL.MCLKCTRLA = 0x00;                           // internal 20MHz clock
+    // CLKCTRL.MCLKCTRLB = 0x00;                           // no prescaler
+    EVSYS.CHANNEL4 = EVSYS_GENERATOR_PORT1_PIN4_gc;     // Pin 6 (PF4) set as capture pin
+    EVSYS.USERTCB0 = EVSYS_CHANNEL_CHANNEL4_gc;         // TCB0 user connected to event channel4
+    TCB0.EVCTRL = TCB_CAPTEI_bm | TCB_EDGE_bm;          // enable input capture on falling edge 
+    TCB0.INTCTRL = TCB_CAPT_bm;                         // enable input capture interrupt
+    TCB0.CTRLB = TCB_CNTMODE_FRQ_gc;                    // frequency measurement mode               
+    TCB0.CTRLA = TCB_CLKSEL_CLKTCA_gc;                  // use TCA timer prescaler 64
 }
